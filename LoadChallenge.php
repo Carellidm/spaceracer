41fa7be4fa0dc670b576aceea94b7759651ba991
Dominic Carelli <49624165+Carellidm@users.noreply.github.com>
2021-02-22 15:58:26 -0500

    Add files via upload

--- LoadChallenge.php
+++ LoadChallenge.php
@@ -8,7 +8,7 @@
 	//Initializes MySQLi
 	$conn = mysqli_init();
 
-	mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+	mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 
 	// Establish the connection
 	mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
