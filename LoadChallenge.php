98d5b12c760c6a88ed02659501289ee23d5e7252
Dominic Carelli <49624165+Carellidm@users.noreply.github.com>
2021-02-22 16:11:52 -0500

    Update LoadChallenge.php

--- LoadChallenge.php
+++ LoadChallenge.php
@@ -8,7 +8,7 @@
 	//Initializes MySQLi
 	$conn = mysqli_init();
 
-	mysqli_ssl_set($conn,NULL,NULL, "/spaceracer/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+	mysqli_ssl_set($conn,NULL,NULL, "/Carellidm/spaceracer/blob/main/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 
 	// Establish the connection
 	mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
