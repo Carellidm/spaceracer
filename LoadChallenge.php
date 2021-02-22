c3a0f44c2ff23c8e77cfb961ad2ee0d056fa0301
Dominic Carelli <49624165+Carellidm@users.noreply.github.com>
2021-02-22 11:36:38 -0500

    Add files via upload

---
+++ LoadChallenge.php
@@ -0,0 +1,60 @@
+<?php
+
+	$host = 'spaceracer.database.windows.net';
+	$username = 'carellidm@spaceracer';
+	$password = 'LostAnderson89e';
+	$db_name = 'spaceracer';
+
+	//Initializes MySQLi
+	$conn = mysqli_init();
+
+	mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+	// Establish the connection
+	mysqli_real_connect($conn, $host, $username, $password, 'quickstartdb', 3306, NULL, MYSQLI_CLIENT_SSL);
+
+	//If connection failed, show the error
+	if (mysqli_connect_errno($conn))
+	{
+	    die('Failed to connect to MySQL: '.mysqli_connect_error());
+	}
+
+	//get the information passed from the unity c# script and store them as variables for accessability
+	$trackid = $_POST["trackId"];
+	$usedchallengeids = $_POST["UsedChallengeIds"];
+	$vpmin = $_POST["vpMin"];
+	$vpmax = $_POST["vpMax"];
+
+	//convert the used challenge ids string into an array it is delimited by ~
+	$usedchallengeidsarray = array_map('intval', explode(",", $usedchallengeids));
+	$usedchallengeidsarray = implode("','", $usedchallengeidsarray);
+	//$usedchallengeidsarray = explode("~", $usedchallengeids);
+	//AND !in_array(challengeId, '" . $usedchallengeidsarray . "') 
+
+	//check to see that the challengeId doesnt already exist using query language
+	$trackidsearchquery = "SELECT * FROM challenge WHERE TrackId = '" . $trackid . "' AND ChallengeId NOT IN ('" . $usedchallengeidsarray . "') AND ReplayVehiclePower >= '" . $vpmin . "' AND ReplayVehiclePower <= '" . $vpmax . "';";
+	
+
+	//run the track id search query to check for any challenges with the matching trackId
+	$result = mysqli_query($conn, $trackidsearchquery) or die("2"); //error code #2 - challenge id check query failed 
+
+	$row = mysqli_fetch_assoc($result);
+
+	$challengeId = $row["ChallengeId"]; 
+	$trackid = $row["TrackId"];
+	$replaybodylevel = $row["ReplayBodyLevel"];
+	$replayenginelevel = $row["ReplayEngineLevel"];
+	$replaytirelevel = $row["ReplayTireLevel"];
+	$replayafterburnerlevel = $row["ReplayAfterburnerLevel"];
+	$replayvehiclePower = $row["ReplayReplayVehiclePower"];
+	$replaymainbodymaterialid = $row["ReplayMainBodyMaterialId"];
+	$replaysecondarybodymaterialid = $row["ReplaySecondaryBodyMaterialId"];
+	$replaybodypositions = $row["ReplayBodyPositions"];
+	$replaybodyrotations = $row["ReplayBodyRotations"];
+	$replayboostconditions = $row["ReplayBoostConditions"];
+	$replaytime = $row["ReplayTime"];
+
+	echo "" . $challengeId . "~" . $trackId . "~" . $replaybodylevel . "~" . $replayenginelevel . "~" . $replaytirelevel . "~" . $replayafterburnerlevel . "~" . $replaymainbodymaterialid . "~" . $replaysecondarybodymaterialid . "~" . $replaybodypositions . "~" . $replaybodyrotations . "~" . $replayboostconditions . "~" . $replaytime . "~";
+	
+	mysqli_close($conn);
+?>
---
+++ SaveChallenge.php
@@ -0,0 +1,39 @@
+<?php
+
+	//create a connection to data base by creating a variable to stroe that connection in 
+	//                      location, username, password, database name 
+	$con = mysqli_connect('localhost', 'root', 'root', 'spaceracer');	
+
+	//check that the connection was successful if not return error code 1
+	if(mysqli_connect_errno())
+	{
+		echo "1: Connection failed"; //error code #1 = connection failed
+		exit();
+	}
+
+	//get the information passed from the unity c# script and store them as variables for accessability
+	//$challengeid = $_POST["challengeId"];
+	$trackid = $_POST["trackId"];
+	$replaybodylevel = $_POST["replayBodyLevel"];
+	$replayenginelevel = $_POST["replayEngineLevel"];
+	$replaytirelevel = $_POST["replayTireLevel"];
+	$replayafterburnerlevel = $_POST["replayAfterburnerLevel"];
+	$replayvehiclepower = $_POST["replayVehiclePower"];
+	$replaymainbodymaterialid = $_POST["replayMainBodyMaterialId"];
+	$replaysecondarybodymaterialid = $_POST["replaySecondaryBodyMaterialId"];
+	$replaybodypositions = $_POST["replayBodyPositions"];
+	$replaybodyrotations = $_POST["replayBodyRotations"];
+	$replayboostconditions = $_POST["replayBoostConditions"];
+	$replaytime = $_POST["replayTime"];
+
+	//add a challenge to the table anything adding to the database is called a query
+	$insertchallengequery = "INSERT INTO challenge (TrackId, ReplayBodyLevel, ReplayEngineLevel, ReplayTireLevel, ReplayAfterburnerLevel, ReplayVehiclePower, ReplayMainBodyMaterialId, ReplaySecondaryBodyMaterialId, ReplayBodyPositions, ReplayBodyRotations, ReplayBoostConditions, ReplayTime) VALUES ('" . $trackid . "','" . $replaybodylevel . "','" . $replayenginelevel . "','" . $replaytirelevel . "','" . $replayafterburnerlevel . "','" . $replayvehiclepower . "','" . $replaymainbodymaterialid . "','" . $replaysecondarybodymaterialid . "','" . $replaybodypositions . "','" . $replayboostconditions . "','" . $replaybodyrotations . "','" . $replaytime . "');";
+
+	//execute the query
+	mysqli_query($con, $insertchallengequery) or die("4: Insert challenge query failed");
+
+	echo("0");
+
+	mysql_close($con);
+
+?>
