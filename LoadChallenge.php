1c38f8e478b5f69557b7cc2c2b7c2cb1d7b98ef8
Dominic Carelli <49624165+Carellidm@users.noreply.github.com>
2021-02-22 15:56:18 -0500

    Add files via upload

--- LoadChallenge.php
+++ LoadChallenge.php
@@ -6,10 +6,15 @@
 	$db_name = 'spaceracerdatabase';
 
 	//Initializes MySQLi
-	$conn = mysqli_connect('spaceracerserver.mysql.database.azure.com', 'Spaceraceradmin@spaceracerserver', 'LostAnderson89e', 'spaceracerdatabase');
+	$conn = mysqli_init();
+
+	mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+	// Establish the connection
+	mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
 
 	//If connection failed, show the error
-	if (mysqli_connect_errno())
+	if (mysqli_connect_errno($conn))
 	{
 	    die('Failed to connect to MySQL: '.mysqli_connect_error());
 	}
--- SaveChallenge.php
+++ SaveChallenge.php
@@ -2,12 +2,23 @@
 
 	//create a connection to data base by creating a variable to stroe that connection in 
 	//                      location, username, password, database name 
-	$conn = mysqli_connect('spaceracerserver.mysql.database.azure.com', 'Spaceraceradmin@spaceracerserver', 'LostAnderson89e', 'spaceracerdatabase');
+	$host = 'spaceracerserver.mysql.database.azure.com';
+	$username = 'Spaceraceradmin@spaceracerserver';
+	$password = 'LostAnderson89e';
+	$db_name = 'spaceracerdatabase';
 
-	//check that the connection was successful if not return error code 1
-	if(mysqli_connect_errno())
+	//Initializes MySQLi
+	$conn = mysqli_init();
+
+	mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+	// Establish the connection
+	mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+	//If connection failed, show the error
+	if (mysqli_connect_errno($conn))
 	{
-		die('Failed to connect to MySQL: '.mysqli_connect_error());
+	    die('Failed to connect to MySQL: '.mysqli_connect_error());
 	}
 
 	//get the information passed from the unity c# script and store them as variables for accessability
