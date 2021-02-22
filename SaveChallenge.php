23bae5389e56c730427c134b3ab09fa917d70971
Dominic Carelli <49624165+Carellidm@users.noreply.github.com>
2021-02-22 16:40:39 -0500

    Add files via upload

--- SaveChallenge.php
+++ SaveChallenge.php
@@ -26,7 +26,7 @@
 	$replaytime = $_POST["replayTime"];
 
 	//add a challenge to the table anything adding to the database is called a query
-	$insertchallengequery = "INSERT INTO challenge (TrackId, ReplayBodyLevel, ReplayEngineLevel, ReplayTireLevel, ReplayAfterburnerLevel, ReplayVehiclePower, ReplayMainBodyMaterialId, ReplaySecondaryBodyMaterialId, ReplayBodyPositions, ReplayBodyRotations, ReplayBoostConditions, ReplayTime) VALUES ('" . $trackid . "','" . $replaybodylevel . "','" . $replayenginelevel . "','" . $replaytirelevel . "','" . $replayafterburnerlevel . "','" . $replayvehiclepower . "','" . $replaymainbodymaterialid . "','" . $replaysecondarybodymaterialid . "','" . $replaybodypositions . "','" . $replayboostconditions . "','" . $replaybodyrotations . "','" . $replaytime . "');";
+	$insertchallengequery = "INSERT INTO challenge (TrackId, ReplayBodyLevel, ReplayEngineLevel, ReplayTireLevel, ReplayAfterburnerLevel, ReplayVehiclePower, ReplayMainBodyMaterialId, ReplaySecondaryBodyMaterialId, ReplayBodyPositions, ReplayBodyRotations, ReplayBoostConditions, ReplayTime) VALUES ('" . $trackid . "','" . $replaybodylevel . "','" . $replayenginelevel . "','" . $replaytirelevel . "','" . $replayafterburnerlevel . "','" . $replayvehiclepower . "','" . $replaymainbodymaterialid . "','" . $replaysecondarybodymaterialid . "','" . $replaybodypositions . "','" . $replaybodyrotations . "','" . $replayboostconditions . "','" . $replaytime . "');";
 
 	//execute the query
 	mysqli_query($conn, $insertchallengequery) or die("4: Insert challenge query failed");
