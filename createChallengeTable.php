58b3b55b0aca54e3a96a1606aac17d4c493174cf
Dominic Carelli <49624165+Carellidm@users.noreply.github.com>
2021-02-22 14:31:06 -0500

    Add files via upload

---
+++ createChallengeTable.php
@@ -0,0 +1,24 @@
+<?php
+
+	$host = 'spaceracerserver.mysql.database.azure.com';
+	$username = 'Spaceraceradmin@spaceracerserver';
+	$password = 'LostAnderson89e';
+	$db_name = 'spaceracer';
+
+	//Initializes MySQLi
+	$conn = mysqli_init();
+
+	mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+	// Establish the connection
+	mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+	//If connection failed, show the error
+	if (mysqli_connect_errno($conn))
+	{
+	    die('Failed to connect to MySQL: '.mysqli_connect_error());
+	}
+
+	echo("3");
+	mysqli_close($conn);
+?>
